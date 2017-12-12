38ebbb7f56872feb6d0b59b6eb24919fd7d9b613
123chenli <2394987221@qq.com>
2017-12-12 10:36:42 +0800

    sift示例

--- expsift.py
+++ expsift.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 from PIL import Image
 from pylab import *
 from numpy import *
@@ -17,8 +18,8 @@ def process_image(imagename, resultname, params='--edge-thresh 10 --peak-thresh 
         im = Image.open(imagename).convert('L')
         im.save('tmp.pgm')
         imagename = 'tmp.pgm'
-    cmmd = str('sift' + imagename + '--output' + resultname + '' + params)
-    os.system(cmmd)
+    cmmd = str('sift ' + imagename + '--output' + resultname + '  ' + params)
+    # os.system(cmmd)
     print('processed', imagename, 'to', resultname)
 
 
@@ -65,3 +66,120 @@ def plot_features(im, locs, circle=False):
     else:
         plot(locs[:, 0], locs[:, 1], 'ob')
     axis('off')
+
+
+def match(desc1, desc2):
+    '''
+    对于第一幅图像中的每个描述子，选取其在第二幅图像中的匹配
+    输入：desc1(第一幅图像中的描述子),desc2(第二幅图像中的描述子)
+    :param desc1:
+    :param desc2:
+    :return:
+    '''
+    desc1 = array([d/linalg.norm(d) for d in desc1])
+    desc2 = array([d/linalg.norm(d) for d in desc2])
+
+    dist_ratio = 0.6
+    desc1_size = desc1.shape
+    matchscores = zeros((desc1_size[0], 1), 'int')
+    desc2t = desc2.T  # 预先计算矩阵转置
+    for i in range(desc1_size[0]):
+        dotprods = dot(desc1[i, :], desc2t)  # 向量点乘
+        dotprods = 0.9999 * dotprods
+        # 反余弦和反排序， 返回第二幅图像中特征的索引
+        indx = argsort(arccos(dotprods))
+        # 检查最近邻的角度是否小于dist_ratio乘以第二近邻的角度
+        if arccos(dotprods)[indx[0]] < dist_ratio * arccos(dotprods)[indx[1]]:
+            matchscores[i] = int(indx[0])
+    return matchscores
+
+
+def match_twosided(desc1, desc2):
+    '''
+    双向对称版本的match()
+    :param desc1:
+    :param desc2:
+    :return:
+    '''
+    matches_12 = match(desc1, desc2)
+    matches_21 = match(desc2, desc1)
+    ndx_12 = matches_12.nonzero()[0]
+
+    # 去掉不对称的点
+    for n in ndx_12:
+        if matches_21[int(matches_12[n])] != n:
+            matches_12[n] = 0
+    return matches_12
+
+
+def appendimages(im1, im2):
+    '''
+    返回将两幅图像并排拼接成的一幅图像
+    :param im1:
+    :param im2:
+    :return:
+    '''
+    # 选取具有最少行数的图像，然后填充充足的行数
+    rows1 = im1.shape[0]
+    rows2 = im2.shape[0]
+
+    if rows1 < rows2:
+        im1 = concatenate((im1, zeros((rows2-rows1, im1.shape[1]))), axis=0)
+    elif rows1 > rows2:
+        im2 = concatenate((im2, zeros((rows1-rows2, im2.shape[1]))), axis=0)
+    return concatenate((im1, im2), axis=0)
+
+
+def plot_matches(im1, im2, locs1, locs2, matchscores, show_below=True):
+    '''
+    显示一幅带有链接匹配之间连线的图片
+    输入:im1, im2(数组图像), locs1, locs2(特征位置),matchscores(match()输出),
+    show_below(如果图像应该显示在匹配的下方)
+    :param im1:
+    :param im2:
+    :param locs1:
+    :param locs2:
+    :param matchscores:
+    :param show_below:
+    :return:
+    '''
+    im3 = appendimages(im1, im2)
+    if show_below:
+        im3 = vstack((im3, im3))
+    imshow(im3)
+    cols1 = im1.shape[1]
+    for i in range(len(matchscores)):
+        if matchscores[i] > 0:
+            plot([locs1[i, 0], locs2[matchscores[i, 0], 0]+cols1], [locs1[i, 1], locs2[matchscores[i, 0], 1]])
+        axis('off')
+
+
+# 示例
+im1f = './images/crans_1_small.jpg'
+im2f = './images/crans_2_small.jpg'
+im1 = array(Image.open(im1f))
+im2 = array(Image.open(im2f))
+
+
+process_image(im1f, 'out_sift_1.txt')
+# 未完成
+l1, d1 = read_features_from_file('out_sift_1.txt')
+figure()
+gray()
+subplot(121)
+plot_features(im1, l1, circle=False)
+
+
+process_image(im2f, 'out_sift_2.txt')
+l2, d2 = read_features_from_file('out_sift_2.txt')
+figure()
+gray()
+subplot(122)
+plot_features(im2, l2, circle=False)
+
+matches = match_twosided(d1, d2)
+print('{} matches'.format(len(matches.nonzero()[0])))
+figure()
+gray()
+plot_matches(im1, im2, l1, l2, matches, show_below=True)
+show()
