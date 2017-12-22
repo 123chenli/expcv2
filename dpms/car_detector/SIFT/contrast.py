6c758bcb0c8711a3bd5c4a0b1276146f2aa93e43
123chenli <2394987221@qq.com>
2017-12-22 15:56:46 +0800

    增加sift算法

---
+++ dpms/car_detector/SIFT/contrast.py
@@ -0,0 +1,20 @@
+import numpy
+
+
+class contrast(object):
+    def lowcontrast(self, arr):
+
+        H, W = arr.shape
+        re = []
+
+        for i in range(1, H - 1):
+            for j in range(1, W - 1):
+                temp = arr[i - 1:i + 2, j - 1:j + 2]
+                winStd = temp.std()
+                winMean = temp.mean()
+                if winStd == 0:
+                    continue
+                elif abs((arr[i][j] - winMean) / winStd) > 0.3:
+                    re.append((i, j))
+
+        return re
